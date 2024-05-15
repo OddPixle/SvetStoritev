ceadd18eb9951c1d61f0b996b9e146cf35bcf292
OddPixle <ziga.ban10@gmail.com>
2024-05-15 08:16:12 +0200

    Create true_index.php

---
+++ true_index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Žiga Ban</title>
+</head>
+<body>
+    <div>
+    Spletna stran podjetja:<a href="dir/index.php">Muhovic</a>
+    Moj porjekt: <a href="svetStoritev/index.php">Svet storitev</a>
+
+    </div>
+</body>
+</html>
